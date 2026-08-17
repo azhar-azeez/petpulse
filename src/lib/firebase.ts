b93b8a926c5a37821e78c15d61b13b7e8fd01e61
azhar-azeez <azharazeez49@gmail.com>
2026-08-17 21:43:17 +0530

    chore: enforce env file ignoring and add env.example template

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,14 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB_q_SWfFDoSN8oboWWw8KzohrPno6nX3o",
-  authDomain: "petpulse-1e2b4.firebaseapp.com",
-  projectId: "petpulse-1e2b4",
-  storageBucket: "petpulse-1e2b4.firebasestorage.app",
-  messagingSenderId: "796583053288",
-  appId: "1:796583053288:web:e8b338e62dfa37fcc4c65d",
-  measurementId: "G-Q0NB03T4N2"
-}
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
 
